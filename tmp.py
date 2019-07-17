c6daab294e93d5994e1f9a9b075cd3227aef9934
aeksei <aeksei94@gmail.com>
2019-07-17 13:45:27 +0300

    calculate code in 10004000 with numpy

--- tmp.py
+++ tmp.py
@@ -2,44 +2,36 @@ import numpy as np
 
 v8 = 0x19a68495
 v6 = 0x19a68495
-v5 = 0
-args = np.array([v8, v6, v5], dtype=np.uint64)
+a3 = [0x01071984, 0x12221959, 0x12301958, 0x03191960]
 
 
-a3 = 0x01071984  # const
+args = np.array([v8, v6, 0], dtype=np.uint32)
 
-args[0] += (a3 + 4 * (args[2] & 3)) + (args[2] ^ args[1]) + ((args[1] >> 5) ^ 16 * args[1])
 
-v5 -= 0x61c88647
-"""
-a = (v8 * 16) & 0xFFFFFFFF
-#print("a =", hex(a))
-b = (v8 >> 5) & 0xFFFFFFFF
-#print("b =", hex(b))
-ab = (a ^ b) & 0xFFFFFFFF
-#print("ab =", hex(ab))
+# args[0] += (a3 + 4 * (args[2] & 3)) + (args[2] ^ args[1]) + ((args[1] >> 5) ^ 16 * args[1])
 
-c = (v5 ^ v8) & 0xffffffff
-#print("c =", hex(c))
+k1 = a3 + np.left_shift(np.bitwise_and(args[2], 3), 2)
 
-p = (ab + c) & 0xffffffff
-#print("p =", hex(p))
+k2 = np.bitwise_xor(args[2], args[1])
 
-d = (v5 >> 11) & 0xffffffff
-#print("d =", hex(d))
-d = (d & 3) & 0xffffffff
-#print("d =", hex(d))
-d = (4 * d) & 0xffffffff
-d = (a3 + d) & 0xffffffff
-k = (p + d) & 0xfffffff
-#print("k =", hex(k))
-#print("k =", hex(k))
+k3 = np.bitwise_xor(np.right_shift(args[1], 5),
+                    np.left_shift(args[1], 4))
 
-v6 += k
-"""
-print(hex(args[0]))
-print(hex(v5))
+args[0] = np.bitwise_and(args[0] + (k1 + k2 + k3), 0xffffffff)
+
+args[2] -= 0x61c88647
+print(args[2])
+k3 = np.bitwise_xor(np.right_shift(args[0], 5),
+                    np.left_shift(args[0], 4))
+
+k2 = np.bitwise_xor(args[2], args[0])
+
+k1 = a3 + np.left_shift(np.bitwise_and(np.right_shift(args[2], 11), 3), 2)
+
+
+
 
 # 0x577c46b1
+8200-000320-027
 
 
