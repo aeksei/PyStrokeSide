ba2aa809f78f78dcc03af512432060335018d58d
aeksei <aeksei94@gmail.com>
2019-07-17 14:37:08 +0300

    final auth code for pm5

--- tmp.py
+++ tmp.py
@@ -1,37 +1,19 @@
 import numpy as np
 
-v8 = 0x19a68495
-v6 = 0x19a68495
-a3 = [0x01071984, 0x12221959, 0x12301958, 0x03191960]
+v8 = np.uint32(0x19a68495)
+v6 = np.uint32(0x19a68495)
+v5 = np.uint32(0)
+a3 = [np.uint32(0x01071984),
+      np.uint32(0x12221959),
+      np.uint32(0x12301958),
+      np.uint32(0x03191960)]
 
+for i in range(32):
+    v8 = np.uint32(v8 + a3[v5 & 3] + (v5 ^ v6) + ((v6 >> 5) ^ np.uint32(16 * v6)))
+    v5 = np.uint32(v5 - 1640531527)
+    v6 = np.uint32(v6 + a3[(v5 >> 11) & 3] + (v5 ^ v8) + ((v8 >> 5) ^ np.uint32(16 * v8)))
 
-args = np.array([v8, v6, 0], dtype=np.uint32)
-
-
-# args[0] += (a3 + 4 * (args[2] & 3)) + (args[2] ^ args[1]) + ((args[1] >> 5) ^ 16 * args[1])
-
-k1 = a3 + np.left_shift(np.bitwise_and(args[2], 3), 2)
-
-k2 = np.bitwise_xor(args[2], args[1])
-
-k3 = np.bitwise_xor(np.right_shift(args[1], 5),
-                    np.left_shift(args[1], 4))
-
-args[0] = np.bitwise_and(args[0] + (k1 + k2 + k3), 0xffffffff)
-
-args[2] -= 0x61c88647
-print(args[2])
-k3 = np.bitwise_xor(np.right_shift(args[0], 5),
-                    np.left_shift(args[0], 4))
-
-k2 = np.bitwise_xor(args[2], args[0])
-
-k1 = a3 + np.left_shift(np.bitwise_and(np.right_shift(args[2], 11), 3), 2)
-
-
-
-
-# 0x577c46b1
-8200-000320-027
+print(hex(v8))
+print(hex(v6))
 
 
